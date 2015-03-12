f4b710cacb32172852df752e3f3c9ab7fcb77c6c
rahul <rahulunair@gmail.com>
2015-03-12 00:15:36 -0500

    progress on the calculator

--- app/src/main/java/com/rahul/simplecalc/HistoryActivity.java
+++ app/src/main/java/com/rahul/simplecalc/HistoryActivity.java
@@ -1,50 +1,110 @@
 package com.rahul.simplecalc;
 
 import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 
 public class HistoryActivity extends ActionBarActivity {
 
-    ArrayList <String> hist = getIntent().getStringArrayListExtra("history");
+    ArrayList <String> hist;
     TextView edit;
+    //SharedPreferences saved_history;
+    String hist_Stringified;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
-       edit = (TextView)findViewById(R.id.text_input);
-
-
+        hist = getIntent().getStringArrayListExtra("history");
+        edit = (TextView)findViewById(R.id.text_input);
+        edit.setTypeface(Typeface.SANS_SERIF);
+        edit.setTextSize(15);
+        edit.setTextColor(Color.BLUE);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        edit.setText(hist.toString());
+        hist_Stringified += hist.toString().replace("[", "").replace("]", "");
+        if (hist_Stringified == "" || hist_Stringified == null)
+            hist_Stringified = readFromFile();
+       // hist_Stringified = saved_history.getString("history", null);
+        edit.setText(hist_Stringified);
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-
-
+       // SharedPreferences.Editor editor = saved_history.edit();
+        //editor.putString("history", TextUtils.join(",", hist));
+        //editor.commit();
+        writeToFile(hist_Stringified);
 
     }
+
+
+
+    private void writeToFile(String data) {
+
+        // persistant storage to a file history.txt
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("history.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+
+    private String readFromFile() {
+        // persistant storage read from a file history.txt
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = openFileInput("history.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
 
 }
--- app/src/main/java/com/rahul/simplecalc/MainActivity.java
+++ app/src/main/java/com/rahul/simplecalc/MainActivity.java
@@ -1,15 +1,13 @@
 package com.rahul.simplecalc;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -21,8 +19,8 @@ import mathparser.ParsingException;
 
 public class MainActivity extends ActionBarActivity {
 
-
-    ArrayList <String> history = new ArrayList<>();
+    SharedPreferences save_memory_variables;
+    ArrayList<String> history = new ArrayList<>();
     Character op = ' ';
     String input_expr = "";
     int result, sum;
@@ -41,6 +39,7 @@ public class MainActivity extends ActionBarActivity {
 
         setContentView(R.layout.activity_main);   // relative layout
         display = (EditText) findViewById(R.id.result_id);
+        history.add("");
 
 
     }
@@ -49,50 +48,50 @@ public class MainActivity extends ActionBarActivity {
     public void number_click(View v) {
         // Displays clicked numbers
 
-       switch (v.getId()) {
-           case R.id.Btn_0_id:
-               show("0");
-               break;
-           case R.id.Btn_1_id:
-               show("1");
-               break;
-           case R.id.Btn_2_id:
-               show("2");
-               break;
-           case R.id.Btn_3_id:
-               show("3");
-               break;
-           case R.id.Btn_4_id:
-               show("4");
-               break;
-           case R.id.Btn_5_id:
-               show("5");
-               break;
-           case R.id.Btn_6_id:
-               show("6");
-               break;
-           case R.id.Btn_7_id:
-               show("7");
-               break;
-           case R.id.Btn_8_id:
-               show("8");
-               break;
-           case R.id.Btn_9_id:
-               show("9");
-               break;
-           case R.id.Btn_dot_id:
-               if(dotFlag == false) {
-                   show(".");
-                   dotFlag =true;
-               }
-                else Toast.makeText(this, "more than one decimal specifier, not allowed", Toast.LENGTH_SHORT).show();
-               break;
-       }
+        switch (v.getId()) {
+            case R.id.Btn_0_id:
+                show("0");
+                break;
+            case R.id.Btn_1_id:
+                show("1");
+                break;
+            case R.id.Btn_2_id:
+                show("2");
+                break;
+            case R.id.Btn_3_id:
+                show("3");
+                break;
+            case R.id.Btn_4_id:
+                show("4");
+                break;
+            case R.id.Btn_5_id:
+                show("5");
+                break;
+            case R.id.Btn_6_id:
+                show("6");
+                break;
+            case R.id.Btn_7_id:
+                show("7");
+                break;
+            case R.id.Btn_8_id:
+                show("8");
+                break;
+            case R.id.Btn_9_id:
+                show("9");
+                break;
+            case R.id.Btn_dot_id:
+               // if (dotFlag == false) {
+                    show(".");
+                 //   dotFlag = true;
+                //} else
+                  //  Toast.makeText(this, "more than one decimal specifier, not allowed", Toast.LENGTH_SHORT).show();
+                break;
+        }
 
     }
 
 
-    public void mem_click(View v){
+    public void mem_click(View v) {
         // memory functions
 
         switch (v.getId()) {
@@ -117,71 +116,82 @@ public class MainActivity extends ActionBarActivity {
             case R.id.Btn_m_plus_id:
                 Log.d(TAG, memSave);
                 saveHistory("M+: ");
-                //operate_mem("-", memSave);
+                if(memSave!= "") show("+"+memSave);
                 break;
             case R.id.Btn_m_minus_id:
-                //operate_mem("+", memSave);
+                if(memSave!= "") show("+"+memSave);
                 Log.d(TAG, memSave);
-                saveHistory("M-: ");
+                saveHistory("M+: ");
                 break;
         }
     }
 
-    public void parseMath (){
+    public void parseMath() {
         // parser to calculate math
         Parser parser = new Parser();
+        String[] operands;
+        String oper1;
+        String oper2;
         try {
-            if(input_expr.contains("√")) {
-                String [] operands = input_expr.split("√");
-                String oper1 = "";
-                String oper2 = "";
-                if(operands[0]!= "" || operands[0] != null) {
+            if (input_expr.contains("√")) {
+                operands = input_expr.split("√");
+                if (operands[0] != "" || operands[0] != null) {
                     oper1 = operands[0].split("[^\\w\\.]+")[operands[0].split("[^\\w\\.]+").length - 1]; // the last element of the array
                     oper2 = operands[1].split("[^\\w\\.]+")[0]; // The first element of the the  array of non special characters
                     input_expr = input_expr.substring(0, input_expr.lastIndexOf(oper1));
                     input_expr += "power(" + oper2 + ",1/" + oper1 + ")";
                 }
             }
+                if (input_expr.contains("P")) {
+                    operands = input_expr.split("P");
+                    if (operands[0] != "" || operands[0] != null) {
+                        oper1 = operands[0].split("[^\\w\\.]+")[operands[0].split("[^\\w\\.]+").length - 1]; // the last element of the array
+                        oper2 = operands[1].split("[^\\w\\.]+")[0]; // The first element of the the  array of non special characters
+                        input_expr = input_expr.substring(0, input_expr.lastIndexOf(oper1));
+                        input_expr += "power(" + oper1+ ","+oper2 + ")";
+                    }
 
-            Log.d ("input_expr", input_expr);
+            }
+
+            Log.d("input_expr", input_expr);
             parser.parse(input_expr);
             solution = parser.getNumericAnswer();
             reset();
             show(Double.toString(solution));
-        } catch(ParsingException ex) {
+        } catch (ParsingException ex) {
             Toast.makeText(this, "Illegal operation", Toast.LENGTH_SHORT).show();
             reset();
             Log.e("Error in Parsing", ex.toString());
         }
     }
 
-    public void operation_click(View v){
+    public void operation_click(View v) {
         // all math operations
         String local = input_expr;
-        String [] ops = input_expr .split("[^\\w\\.]+");
-        Log.d("split_expr",Arrays.toString(ops) );
-        switch (v.getId()){
+        String[] ops = input_expr.split("[^\\w\\.]+");
+        Log.d("split_expr", Arrays.toString(ops));
+        switch (v.getId()) {
             case R.id.Btn_equal_id:
                 saveHistory("=");
                 parseMath();
                 break;
             case R.id.Btn_c_id:
                 reset();
+                saveHistory("C");
                 break;
             case R.id.Btn_ce_id:
-                if(ops.length != 0) {
-                    input_expr = input_expr.replace(ops[ops.length -1], "");
+                if (ops.length != 0) {
+                    input_expr = input_expr.replace(ops[ops.length - 1], "");
                     show("");
-                }
-                else reset();
+                } else reset();
                 break;
             case R.id.Btn_left_id:
-                    if(input_expr.length() >0) {
-                        input_expr = input_expr.substring(0, input_expr.length() - 1);
-                        show("");
-                    }
-                else
-                reset();
+                if (input_expr.length() > 0) {
+                    input_expr = input_expr.substring(0, input_expr.length() - 1);
+                    show("");
+                } else
+                    saveHistory("CE");
+                    reset();
                 break;
             case R.id.Btn_plus_id:
                 show("+");
@@ -198,17 +208,34 @@ public class MainActivity extends ActionBarActivity {
             case R.id.Btn_perc_id:
                 show("%");
                 break;
+            case R.id.button_x_2:
+                reset();
+                show("power(" + local + ", 2)");
+                break;
             case R.id.Btn_sq_id:
                 reset();
-                show("sqrt("+local+")");
+                show("sqrt(" + local + ")");
+                break;
+            case R.id.button_x_3:
+                reset();
+                show("power(" + local + ", 3)");
+                break;
+            case R.id.Btn_plus_minus_id:
+                reset();
+                double l = Double.parseDouble(local);
+                l = 0 - l;
+                show(Double.toString(l));
                 break;
             case R.id.button_3_root_x:
                 reset();
-                show("power("+local+", 1/3)");
+                show("power(" + local + ", 1/3)");
+                break;
             case R.id.button_y_root_x:
                 show("√");
                 break;
             case R.id.Btn_one_by_x_id:
+                reset();
+                show("1/" + local);
                 break;
             case R.id.Btn_l_b:
                 show("(");
@@ -216,44 +243,72 @@ public class MainActivity extends ActionBarActivity {
             case R.id.Btn_r_b:
                 show(")");
                 break;
-            case R.id.button_log:
+            case R.id.Btn_log_id:
+                reset();
+                show("log10(" + local + ")");
                 break;
+            case R.id.Btn_10_x_id:
+                reset();
+                show("power(10," + local + ")");
+                break;
+            case R.id.Btn_pi_id:
+                show("pi");
+                break;
+            case R.id.button_n_fact:
+                reset();
+                show("factorial(" + local + ")");
+                break;
+            case R.id.button_x_y:
+                show("P");
+                break;
+            case R.id.Btn_mod_id:
+                show("%");
 
 
         }
     }
 
-    private void saveHistory (String save){
+    private void saveHistory(String save) {
         history.add(save);
         Log.d("history", history.toString());
     }
 
     private void reset() {
         input_expr = "";
-        op = ' ';
-        result = 0;
-        sum = 0;
         display.setText("");
     }
 
 
     private void show(String value) {
-            input_expr = input_expr + value;
-            Log.d(TAG, value);
-            saveHistory(value);
-            display.setText(input_expr);
+        input_expr = input_expr + value;
+        Log.d(TAG, value);
+        saveHistory(value);
+        display.setText(input_expr);
     }
 
 
-
-    public void show_history(View view)
-    {
+    public void show_history(View view) {
+        System.out.println("entered show history func");
         Intent intent = new Intent(MainActivity.this, HistoryActivity.class);
         intent.putExtra("history", history);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        save_memory_variables = getSharedPreferences("memSave_tag", 0);
+        super.onPause();
+        SharedPreferences.Editor editor = save_memory_variables.edit();
+        editor.putString("memSave_tag", memSave);
+        editor.commit();
 
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        memSave = save_memory_variables.getString("memSave_tag", null);
+    }
 }
 
 
